2e4e33108eda61753d394a8922492db6da25d72e
amalyakubov <129682707+amalyakubov@users.noreply.github.com>
2023-07-07 16:36:46 +0200

    Clean up the project
    Make the screen respond to the restart
    Set player2 variables to default when restarting
    Change game logic to allow player2 to play the game

--- js.js
+++ js.js
@@ -13,12 +13,34 @@ let player =  {
     RESTART: document.getElementById('restart'),
 }
 
-player.SUBMIT.addEventListener('click', (event) => {
-    playerMarker = player.MARKER.value;
-    playerName = player.NAME.value;
-    player1 = new Player(playerName, playerMarker);
+let Player2 = {
+    playerName: null,
+    playerMarker: null,
+    NAME: document.getElementById('player_2_name'),
+    SUBMIT: document.getElementById('submit-2')
+}
+
+Player2.SUBMIT.addEventListener('click', (event) => {
+    Player2.playerName = Player2.NAME.value;
     event.preventDefault();
-});
+})
+
+player.SUBMIT.addEventListener('click', (event) => {
+    event.preventDefault();
+    player.playerMarker = player.MARKER.value;
+    gameBoard.playerMarker = player.MARKER.value;
+    playerName = player.NAME.value;
+    player1 = new Player(player.playerName, player.playerMarker);
+    document.getElementById('player-form').style.display = 'none';
+    document.getElementById('submit').style.display = 'none';
+    document.getElementById('player-2-form').style.display = 'flex';
+    document.getElementById('player-space').style.height = '150px';
+    if (player.playerMarker === 'X') {
+        Player2.playerMarker = 'O';
+    } else {
+        Player2.playerMarker = 'X';
+    }
+})
 
 let tiles = document.getElementsByClassName('game-tile');
 Array.from(tiles).forEach((tile) => {
@@ -33,14 +55,20 @@ let gameBoard = {
     lastIndex: 0,
     timesInaRow: 0,
     gameboard: ['', '', '', '' ,'' ,'', '', '', ''],
+    playerMarker: null,
     play: function (tilenumber) {
         if (this.emptyCheck(tilenumber) === true) {
             this.lastIndex = tilenumber;
-            this.gameboard[tilenumber] = player1.playerMarker;
-            document.getElementById(`${tilenumber + 1}`).innerHTML = player1.playerMarker;
+            this.gameboard[tilenumber] = this.playerMarker;
+            document.getElementById(`${tilenumber + 1}`).innerHTML = this.playerMarker;
             this.left -= 1;
             this.winCheck();
             this.rowVictoryCheck();
+            if (this.playerMarker === player.playerMarker) {
+                this.playerMarker = Player2.playerMarker;
+            } else {
+                this.playerMarker = player.playerMarker;
+            }
         } else {
             console.log('The section is not empty');
         }
@@ -56,23 +84,23 @@ let gameBoard = {
     winCheck: function() {
         this.rowVictoryCheck();
         if (this.rowVictoryCheck()) {
-            if (this.playerWon) {
-                this.messageElement.textContent = 'Congratulations! You have won!';
+            if (this.playerOneWon) {
+                this.messageElement.textContent = `${player.playerMarker} has won!`;
                 this.messageElement.style.color = 'green';
             } else {
-                this.messageElement.textContent = 'You have lost!';
-                this.messageElement.style.color = 'red';
+                this.messageElement.textContent = `${Player2.playerName} has won!`;
+                this.messageElement.style.color = 'green';
             }
         } else if(this.left === 0) {
             this.messageElement.textContent = 'It\'s a tie';
             this.messageElement.style.color = 'black';
         }
     },
-    playerWon: null,
+    playerOneWon: null,
     messageElement: document.getElementById('message-space'),
     rowCheck: function() {
-        if (this.gameboard[0] === player1.playerMarker && this.gameboard[1] === player1.playerMarker && this.gameboard[2] === player1.playerMarker || this.gameboard[3] === player1.playerMarker && this.gameboard[4] === player1.playerMarker && this.gameboard[5] === player1.playerMarker ||
-        this.gameboard[6] === player1.playerMarker && this.gameboard[7] === player1.playerMarker && this.gameboard[8] === player1.playerMarker || this.gameboard[0] === player1.playerMarker && this.gameboard[3] === player1.playerMarker && this.gameboard[6] === player1.playerMarker || this.gameboard[1] === player1.playerMarker && this.gameboard[4] === player1.playerMarker && this.gameboard[7] === player1.playerMarker || this.gameboard[2] === player1.playerMarker && this.gameboard[5] === player1.playerMarker && this.gameboard[8] === player1.playerMarker) {
+        if (this.gameboard[0] === this.playerMarker && this.gameboard[1] === this.playerMarker && this.gameboard[2] === this.playerMarker || this.gameboard[3] === this.playerMarker && this.gameboard[4] === this.playerMarker && this.gameboard[5] === this.playerMarker ||
+        this.gameboard[6] === this.playerMarker && this.gameboard[7] === this.playerMarker && this.gameboard[8] === this.playerMarker || this.gameboard[0] === this.playerMarker && this.gameboard[3] === this.playerMarker && this.gameboard[6] === this.playerMarker || this.gameboard[1] === this.playerMarker && this.gameboard[4] === this.playerMarker && this.gameboard[7] === this.playerMarker || this.gameboard[2] === this.playerMarker && this.gameboard[5] === this.playerMarker && this.gameboard[8] === this.playerMarker) {
             return true;
         } else {
             return false;
@@ -81,9 +109,9 @@ let gameBoard = {
     rowVictoryCheck: function() {
         if (this.rowCheck() === true) {
             if (this.gameboard[this.lastIndex] === player1.playerMarker) {
-                this.playerWon = true;
+                this.playerOneWon = true;
             } else {
-                this.playerWon = false;
+                this.playerOneWon = false;
             }
             return true;
         } else {
@@ -96,13 +124,20 @@ let gameBoard = {
         this.timesInaRow = 0;
         this.playerWon = null;
         this.left = 9;
+        this.playerMarker = null;
         player.player1 = null;
         player.playerName = null;
         player.playerMarker = null;
+        Player2.playerMarker = null;
+        Player2.playerName = null;
         Array.from(tiles).forEach((tile) => {
             tile.innerHTML = '';
         });
         this.messageElement.textContent = '';
+        document.getElementById('player-form').style.display = 'block';
+        document.getElementById('submit').style.display = 'block';
+        document.getElementById('player-2-form').style.display = 'none';
+        document.getElementById('player-space').style.height = '200px';
     }
 }
 
